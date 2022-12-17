7c5e816e7474e4856e9bcf2a50317cca589aee92
omekagu joseph <omekaguj@gmail.com>
2022-12-17 15:23:28 +0100

    5th commit

--- Components/MobileDropdown.jsx
+++ Components/MobileDropdown.jsx
@@ -3,24 +3,26 @@ import React from 'react';
 const MobileDropdown = () => {
   return (
     <div className="mobileDropdown">
-      <a href="/Home" className="link mb-link">
-        New
-      </a>
-      <a href="/Men" className="link mb-link">
-        Men
-      </a>
-      <a href="/Women" className="link mb-link">
-        Women
-      </a>
-      <a href="/MadetoMeasure" className="link mb-link">
-        made to measure
-      </a>
-      <a href="/Accessories" className="link mb-link">
-        Accessories
-      </a>
-      <a href="/Videos" className="link mb-link">
-        Videos
-      </a>
+      <div className="mobileDropdown__container">
+        <a href="/Home" className="link mb-link">
+          New
+        </a>
+        <a href="/Men" className="link mb-link">
+          Men
+        </a>
+        <a href="/Women" className="link mb-link">
+          Women
+        </a>
+        <a href="/MadetoMeasure" className="link mb-link">
+          made to measure
+        </a>
+        <a href="/Accessories" className="link mb-link">
+          Accessories
+        </a>
+        <a href="/Videos" className="link mb-link">
+          Videos
+        </a>
+      </div>
     </div>
   );
 };
